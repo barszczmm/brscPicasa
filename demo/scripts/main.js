6617a124985843d937faf957051787458c5f9f15
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-08-29 10:26:54 +0200

    demo page updated, jQuery updated, Colorbox updated

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -5,9 +5,12 @@ $(document).ready(function() {
 		photo_displaysize: '320'
 	};
 
+	// link replacing example #1: public albums
 	$('#example1 .trigger').click(
 		function() {
 			$(this).parent().brscPicasa({
+				max_results: 5,
+				start_index: 3,
 				classes: {
 					link_with_image: 'loaded-link'
 				}
@@ -16,6 +19,7 @@ $(document).ready(function() {
 		}
 	);
 
+	// link replacing example #2: public album
 	$('#example2 .trigger').click(
 		function() {
 			$(this).parent().brscPicasa({
@@ -27,6 +31,7 @@ $(document).ready(function() {
 		}
 	);
 
+	// link replacing example #3: private photos
 	$('#example3 .trigger').click(
 		function() {
 			$(this).parent().brscPicasa({
@@ -34,24 +39,32 @@ $(document).ready(function() {
 					link_with_image: 'loaded-link'
 				},
 				callback: function() {
-					$('a.loaded-link', this).colorbox({rel:'example3'});
+					$('a.loaded-link', this).colorbox({
+						rel:'example3',
+						title: function() {
+							return $('img', this).attr('title');
+						}
+					});
 				}
 			});
 			$(this).remove();
 		}
 	);
 
+	// pure options example #1: 50 last user public photos
 	$('#example4 .trigger').click(
 		function() {
 			$(this).parent().brscPicasa({
 				user: 'barszczmm',
 				mode: 'photos',
-				photos_linkedsize: 'none'
+				photos_linkedsize: 'none',
+				max_results: 50
 			});
 			$(this).remove();
 		}
 	);
 
+	// pure options example #2: private photo
 	$('#example5 .trigger').click(
 		function() {
 			$(this).parent().brscPicasa({
