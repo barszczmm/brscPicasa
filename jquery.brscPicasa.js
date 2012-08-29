8f21fff82164912fec046b0d2d0637930a7d7422
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-08-29 10:11:31 +0200

    added max_results and start_index options, added alt and title attributes to all generated img elements

--- jquery.brscPicasa.js
+++ jquery.brscPicasa.js
@@ -9,6 +9,8 @@
 				album_name: '',
 				photo_id: '',
 				authkey: '',
+				max_results: 100,
+				start_index: 1,
 				// thumbsizes: 32, 48, 64, 72, 104, 144, 150, 160 with 'u' or 'c' suffix (for uncropped (default if no suffix added) or cropped version)
 				// sizes: 94, 110, 128, 200, 220, 288, 320, 400, 512, 576, 640, 720, 800, 912, 1024, 1152, 1280, 1440, 1600
 				// othersizes: d - original uploaded photo including all original Exif data, 'none' - photo will not be linked to bigger size
@@ -23,15 +25,16 @@
 				mode: 'from_link', // available modes: albums - list all user public albums, photos - list photos from specified album, photo - display one photo, from_link - find links to pisaweb and guess what to display
 				from_link_target: '', // if empty 'a' element will be replaced with new content, any other string will be treated as jQuery selector and new content will be added to found element using append method
 				classes: {
-					link_replacer: '',
-					container: '',
-					album_list: '',
-					photos_list: '',
-					list_item: '',
-					single_item: '',
-					link_with_image: '',
-					photo: '',
-					title: ''
+					link_replacer: '', // added to <div> that will be inserted in place of <a> element in link replacing mode
+					container: '', // added to <div> that contains all generated code
+					album_list: '', // added to <ul> element that contains list of albums
+					photos_list: '', // added to <ul> element that contains list of photos
+					list_item: '', // added to <li> element that contains album or photo on list
+					thumbnail: '', // added to <img> element that contains thumbnail of album or photo on list
+					single_item: '', // added to <div> element that contains single photo (not on lists)
+					link_with_image: '', // added to <a> element that contains <img> element inside
+					photo: '', // added to <img> element on single photo mode
+					title: '' // added to <span> element that contains title of album or photo
 				},
 				callback: false
 			},
@@ -77,6 +80,8 @@
 							kind: 'album',
 							alt: 'json',
 							thumbsize: o.albums_thumbsize,
+							'max-results': o.max_results,
+							'start-index': o.start_index,
 							fields: 'entry(title,link[@rel=\'alternate\'],media:group/media:thumbnail)',
 							visibility: 'all'
 						},
@@ -100,7 +105,7 @@
 
 							for (var i = 0; i < entries.length; ++i) {
 								var entry = entries[i];
-								$item = $('<li class="' + name + 'ListItem ' + o.classes.list_item + '"><a class="' + name + 'LinkWithImage ' + o.classes.link_with_image + '" href="' + entry.link[0].href + '"><img class="' + name + 'Photo ' + o.classes.thumbnail + '" src="' + entry.media$group.media$thumbnail[0].url + '" width="' + entry.media$group.media$thumbnail[0].width + '" height="' + entry.media$group.media$thumbnail[0].height + '" /></a></li>');
+								$item = $('<li class="' + name + 'ListItem ' + o.classes.list_item + '"><a class="' + name + 'LinkWithImage ' + o.classes.link_with_image + '" href="' + entry.link[0].href + '"><img class="' + name + 'Photo ' + o.classes.thumbnail + '" src="' + entry.media$group.media$thumbnail[0].url + '" width="' + entry.media$group.media$thumbnail[0].width + '" height="' + entry.media$group.media$thumbnail[0].height + '" alt="' + entry.title.$t + '" title="' + entry.title.$t + '" /></a></li>');
 								if (o.albums_titles) {
 									$item.children().append('<span class="' + name + 'Title ' + o.classes.title + '">' + entry.title.$t + '</span>');
 								}
@@ -123,6 +128,8 @@
 							kind: 'photo',
 							alt: 'json',
 							thumbsize: o.photos_thumbsize,
+							'max-results': o.max_results,
+							'start-index': o.start_index,
 							fields: 'entry(title,media:group(media:thumbnail, media:content))',
 							visibility: 'all'
 						},
@@ -156,7 +163,7 @@
 
 							for (var i = 0; i < entries.length; ++i) {
 								var entry = entries[i];
-								$item = $('<li class="' + name + 'ListItem ' + o.classes.list_item + '"><img class="' + name + 'Photo ' + o.classes.thumbnail + '" src="' + entry.media$group.media$thumbnail[0].url + '" width="' + entry.media$group.media$thumbnail[0].width + '" height="' + entry.media$group.media$thumbnail[0].height + '" /></li>');
+								$item = $('<li class="' + name + 'ListItem ' + o.classes.list_item + '"><img class="' + name + 'Photo ' + o.classes.thumbnail + '" src="' + entry.media$group.media$thumbnail[0].url + '" width="' + entry.media$group.media$thumbnail[0].width + '" height="' + entry.media$group.media$thumbnail[0].height + '" alt="' + entry.title.$t + '" title="' + entry.title.$t + '" /></li>');
 								if (o.photos_linkedsize !== 'none') {
 									$item.children().wrap('<a class="' + name + 'LinkWithImage ' + o.classes.link_with_image + '" href="' + entry.media$group.media$content[0].url + '"></a>');
 								}
@@ -218,7 +225,7 @@
 						data,
 						function(root) {
 							var entry = root.entry || {},
-								$div = $('<div class="' + name + 'SingleItem ' + o.classes.single_item + '"><img class="' + name + 'Photo ' + o.classes.photo + '" src="' + entry.media$group.media$thumbnail[0].url + '" width="' + entry.media$group.media$thumbnail[0].width + '" height="' + entry.media$group.media$thumbnail[0].height + '" /></div>');
+								$div = $('<div class="' + name + 'SingleItem ' + o.classes.single_item + '"><img class="' + name + 'Photo ' + o.classes.photo + '" src="' + entry.media$group.media$thumbnail[0].url + '" width="' + entry.media$group.media$thumbnail[0].width + '" height="' + entry.media$group.media$thumbnail[0].height + '" alt="' + entry.title.$t + '" title="' + entry.title.$t + '" /></div>');
 
 							if (o.photo_linkedsize !== 'none') {
 								$div.children().wrap('<a class="' + name + 'LinkWithImage ' + o.classes.link_with_image + '" href="' + entry.media$group.media$content[0].url + '"></a>');
